50ca46c84f61c4f316668425e796a66eb57cffdb
Gustavo Scherer <gst.scherer@gmail.com>
2024-03-06 15:52:43 -0300

    att
    att

--- Java/SprintBoot/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Java/SprintBoot/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,9 +3,13 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -51,5 +55,25 @@ public class Principal {
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
                 .limit(5)
                 .forEach(System.out::println);
+
+        List<Episodio> episodios = listaTemporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios?");
+        var ano = scanner.nextInt();
+        scanner.nextLine();
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento()!=null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e-> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                        " / Episódio: " + e.getTitulo() +
+                        " / Data de lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
